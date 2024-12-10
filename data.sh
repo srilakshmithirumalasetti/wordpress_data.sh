02f272068195c4b11e8480c16de13962bb58ae9c
root <root@ip-172-31-28-194.ec2.internal>
2024-12-10 05:32:30 +0000

    commiting files

---
+++ data.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Update the system
+yum update -y
+yum install -y docker
+systemctl start docker
+systemctl enable docker
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+sudo usermod -aG docker ec2-user
+sudo chmod 666 /var/run/docker.sock
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+systemctl start docker
+cat > /home/ec2-user/docker-compose.yml <<EOL
+version: '3.3'
+services:
+  db:
+    image: mysql:8.0.19
+    command: --default-authentication-plugin=mysql_native_password
+    volumes:
+      - db_data:/var/lib/mysql
+    restart: always
+    environment:
+      - MYSQL_ROOT_PASSWORD=wordpress
+      - MYSQL_DATABASE=databaseword
+      - MYSQL_USER=admin
+      - MYSQL_PASSWORD=admin123
+
+  wordpress:
+    image: wordpress:latest
+    ports:
+      - "80:80"
+    restart: always
+    environment:
+      - WORDPRESS_DB_HOST=db
+      - WORDPRESS_DB_USER=admin
+      - WORDPRESS_DB_PASSWORD=admin123
+      - WORDPRESS_DB_NAME=databaseword
+
+volumes:
+  db_data:
+EOL
+chmod 644 /home/ec2-user/docker-compose.yml
+cd /home/ec2-user
+docker-compose up -d
+docker ps
---
+++ ec2.tf
@@ -0,0 +1,60 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Define a security group for WordPress
+resource "aws_security_group" "wordpress_sg" {
+  name        = "wordpress-sg"
+  description = "Allow HTTP, HTTPS, SSH, and MySQL traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create an EC2 instance for WordPress
+resource "aws_instance" "wordpress" {
+  ami           = "ami-0166fe664262f664c"
+  instance_type = "t2.micro"
+  key_name      = "spandana"              
+  subnet_id     = "subnet-0c85ac199c0740b9f"            
+  security_groups = [aws_security_group.wordpress_sg.id]
+
+  user_data = file("data.sh")            
+
+  tags = {
+    Name = "WordPressServer"
+  }
+}
+
