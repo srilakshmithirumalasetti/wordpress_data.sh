316a461f6fbcb7ef38c3ffd5cf0669742ff345c2
Spandana115 <baindlaspandana0115@gmail.com>
2024-12-11 22:30:02 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,22 +2,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "my_instance" {
-  ami                   ="ami-0166fe664262f664c"
-  instance_type          = "t2.micro"
-  count                  = 1
-  key_name               = "spandana"
-  associate_public_ip_address = true
-  user_data              = file("data.sh")
-  subnet_id = "subnet-0c85ac199c0740b9f"
-  tags = {
-    Name = "WordPressServer"
-  }
-}
-
-resource "aws_security_group" "demosg" {
-  name        = "demosg"
-  description = "Security group for WordPress and MySQL"
+resource "aws_security_group" "wordpress_sg" {
+  name        = "wordpress-sg"
+  description = "Allow HTTP, HTTPS, SSH, and MySQL traffic"
 
   ingress {
     from_port   = 80
@@ -25,11 +12,19 @@ resource "aws_security_group" "demosg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress{
-    from_port=443
-    to_port=443
-    protocol="tcp"
-    cidr_blocks=["0.0.0.0/0"]
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -38,18 +33,26 @@ resource "aws_security_group" "demosg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress{
-   from_port=22
-   to_port=22
-   protocol="tcp"
-   cidr_blocks=["0.0.0.0/0"]
-  }
-
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create an EC2 instance for WordPress
+resource "aws_instance" "wordpress" {
+  ami           = "ami-0166fe664262f664c"
+  instance_type = "t2.micro"
+  key_name      = "spandana"              
+  subnet_id     = "subnet-0c85ac199c0740b9f"            
+  security_groups = [aws_security_group.wordpress_sg.id]
+
+  user_data = file("data.sh")            
+
+  tags = {
+    Name = "WordPressServer"
   }
 }
