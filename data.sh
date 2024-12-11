8558c5b40bc8b4a0e24ab22150d80e47cefdad95
Spandana115 <baindlaspandana0115@gmail.com>
2024-12-11 11:58:56 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -44,3 +44,7 @@ chmod 644 /home/ec2-user/docker-compose.yml
 cd /home/ec2-user
 docker-compose up -d
 docker ps
+TERRAFORM_VERSION="1.5.4" # Change the version as needed
+curl -LO "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
+unzip terraform_${TERRAFORM_VERSION)_linux_amd64.zip
+sudo mv terraform /usr/local/bin
